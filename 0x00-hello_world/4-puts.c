0f7f03a6ffd09b21ba2b41afe08870044878f62a
cristian <cristaker1927@hotmail.com>
2021-09-30 16:09:37 -0700

    programming

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,11 @@
 #include<stdio.h>
-# /**
-#   *
-#   main - Entry point
-#    *
-#     * Return: Always 0 (Succes)
-#      */
-#       int main(void)
-#        {
-#               puts("\"programming is like a multilingual puzzle")
-#                return (0);
-#                 }
+/**
+ *  * main - Entry point
+ *   *
+ *    * Return: Always 0 (Success)
+ *     */
+int main(void)
+{
+		puts("\"Programming is like building a multilingual puzzle");
+			return (0);
+}
