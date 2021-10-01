2ef44ab18941cd0925cf39465a2957fc9fcc558e
cristian <cristaker1927@hotmail.com>
2021-09-30 17:05:11 -0700

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,8 +1,11 @@
 #include<stdio.h>
 /**
-*  first exercise
-*   Result(0)
+*  main - Entry point
+*
+*   Result Always 0 (Succes)
+*
 *    **/
+int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 		result(0);
