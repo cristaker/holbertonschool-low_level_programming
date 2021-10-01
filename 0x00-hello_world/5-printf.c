b6a20dcdefe1ad0139f688fc9680e901e791fa82
cristian <cristaker1927@hotmail.com>
2021-09-30 17:08:58 -0700

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,9 @@
 #include<stdio.h>
 /**
-*  main - Entry point
-*
-*   Result Always 0 (Succes)
-*
-*    **/
+ * * main - Entry point
+ * *  Return: Always (0) (Succes)
+ * *  **/
+
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
