b9d94abe47b080951eb45d9b2520a8ec835d42c7
cristian <cristaker1927@hotmail.com>
2021-10-04 13:43:39 -0700

    point 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * numbers 1-10 in order
+ *
+ * main - Entry point
+ *
+ * Result = Always 0 (Succes)
+ *
+ * **/
+int main(void)
+{
+	int i;
+	for (i = 0; i <= 10; i++)
+	{
+			printf("%i", i);
+		}
+				printf("\n");
+					return (0);
+}
