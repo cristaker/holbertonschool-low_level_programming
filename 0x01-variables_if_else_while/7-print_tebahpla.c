bdc5ba906b73f6e295883d4a750acb520af3242b
cristian <cristaker1927@hotmail.com>
2021-10-04 15:28:22 -0700

    point 7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ *  *  * main - Prints alphabet reverse in lowercase.
+ *   *   *
+ *    *    * Return: Always 0 (Success)
+ *     *     **/
+int main(void)
+{
+	int i;
+		for (i = 96; i < 122; i--)
+{
+			putchar(i);
+}
+				putchar('\n');
+					return (0);
+}
