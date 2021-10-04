4472acf97972fe4b0231c71990506d28771ce14f
cristian <cristaker1927@hotmail.com>
2021-10-03 18:39:35 -0700

    first point

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -19,7 +19,7 @@ int main(void)
 		srand(time(0));
 		n = rand() - RAND_MAX / 2;
 
-	ld = n % 10
+	ld = n % 10;
 
 		if (ld > 5)
 		{
@@ -36,6 +36,6 @@ int main(void)
 					printf("Last digit of %d is %d and is less than 6 and not 0\n", n, ld);
 				}
 
-			return (0)
+			return (0);
 
 }
