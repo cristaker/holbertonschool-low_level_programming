385a4d485f6c8182ce720ac52306fe5f6b1225fc
cristian <cristaker1927@hotmail.com>
2021-10-04 12:58:42 -0700

    point 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Result = Always 0 (Succes)
+ * **/
+int main(void)
+{
+	char alp[24]; "abcdfghijklmnoprstuvwxyz";
+
+		putchar(alp[24]);
+
+			putchar('\n');
+
+				return (0);
+}
