840d4aa8c1e8594f02cdc46b42501479cfec5fd3
cristian <cristaker1927@hotmail.com>
2021-10-04 19:21:46 -0700

    point 8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - print number in base 16 lowercase
+ *
+ * Return = Always 0 (Succes)
+ * **/
+main(void)
+{
+	int i;
+		for (i = 48; i < 58; i++)
+		{
+			putchar (i);
+		}
+			for (i = 97; i < 103; i++)
+			{
+				putchar (i);
+			}
+					putchar ('\n');
+						return (0);
+}
