dd563748e7ab480a9b54a1fd4fa6e97cb5bb48b6
cristian <cristaker1927@hotmail.com>
2021-10-04 20:13:19 -0700

    point 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - all combination of single digit
+ *
+ * Return: Always 0 (Succes)
+ * **/
+int main(void)
+{
+	int i;
+		for (i = 48; i < 58; i++)
+		{
+			putchar (i);
+		}
+			if (i != 57)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+					putchar ('\n');
+						return (0);
+}
