2e1cef00b95a3f9e8ec62744ca2bd6bf1d381300
cristian <cristaker1927@hotmail.com>
2021-10-06 20:37:51 -0700

    point 2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -8,11 +8,11 @@ void print_alphabet_x10(void)
 {
 	int i, e;
 			for (i = 0; i < 10; i++)
-	{
+			{
 				for (e = 97; e < 123; e++)
 	{
 					_putchar (e);
 	}
+				_putchar ('\n');
 	}
-		_putchar ('\n');
 }
