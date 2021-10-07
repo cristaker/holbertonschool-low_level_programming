cc9d430955fe95584967d019d5bf2106ba84a19c
cristian <cristaker1927@hotmail.com>
2021-10-07 09:20:33 -0700

    point 5

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * print_sign - determine greater or lower number
+ * @n: character to check
+ * Return: 1 if 0 is true, 0 if equal 0, -1 if lower 0.
+ *
+ * **/
+int print_sign(int n)
+{
+	if (n > 48)
+	{
+		_putchar (43);
+			return (1);
+	}
+	else if (n < 48)
+	{
+		_putchar (45);
+			return (-1);
+	}
+	else
+	{
+		_putchar (48);
+			return (0);
+	}
+		_putchar ('\n');
+}
