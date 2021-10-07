cfad92cbcf464a8674b8c12b5f9d6e4d22765cbd
cristian <cristaker1927@hotmail.com>
2021-10-07 14:26:27 -0700

    point 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _abs - protitype for return absolute value.
+ * @i: imput integer
+ * Return: Absolute value
+ * **/
+int _abs(int i)
+{
+	if (i > 0)
+	{
+		return (i);
+	}
+	else
+	{
+		return (i * -1);
+	}
+}
