1b0d7a156a1ed11e89617a6c59828f0e0652c54a
cristian <cristaker1927@hotmail.com>
2021-10-08 20:09:23 -0700

    point 3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_numbers - function that print the numbers.
+ * Return: Always 0 (Succes)
+ * **/
+
+void print_numbers(void)
+{
+	int n;
+		for (n = 48; n < 58; n++)
+			{
+			_putchar(n);
+			}
+				_putchar('\n');
+}
