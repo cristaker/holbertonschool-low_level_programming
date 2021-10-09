24fff3151dabe1b9ca50980ac99fbb2f1fafcef8
cristian <cristaker1927@hotmail.com>
2021-10-09 10:36:57 -0700

    point 7

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,34 @@
+#include "main.h"
+/**
+ * print_diagonal - prototype to print
+ * *@n: number of \ to check
+ * * Return: Always 0 (Succes)
+ * *  **/
+
+void print_diagonal(int n)
+{
+	int i, j;
+
+	if (n > 0)
+	{
+		for (i = 0; i < n; i++)
+		{
+			for (j = 0; j <= n; j++)
+			{
+				if (i == j)
+				{
+					_putchar(92);
+				}
+				else
+				{
+					_putchar(32);
+				}
+			}
+			_putchar('\n');
+		}
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
