1c1b53a7225e08c08bb200fa6bd52c58915cd7c3
cristian <cristaker1927@hotmail.com>
2021-10-12 20:08:03 -0700

    point 5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * rev_string - reverse string
+ * @s: integer to check
+ * Return: 0
+ * **/
+
+void rev_string(char *s)
+{
+	int a = 0, b = 0;
+	char scopy[20];
+
+	while (s[a])
+	{
+		scopy[a] = s[a];
+		a++;
+	}
+	a = a - 1;
+	while (a >= 0)
+	{
+		s[a] = scopy[b];
+			a--;
+			b++;
+	}
+}
