3a16b5c8e9ee4aa0768c2b681d2bfe2d7ead5c87
cristian <cristaker1927@hotmail.com>
2021-10-12 21:03:53 -0700

    point 6

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -4,7 +4,24 @@
  * @str: char to integer
  * Return: 0
  * **/
+
 void puts2(char *str)
 {
-	
+
+	int a = 0, b = 0;
+
+	while (str[a])
+	{
+		a++;
+		b++;
+	}
+
+	for (a = 0; a <= b - 1; a++)
+	{
+		if (a % 2 == 0)
+		{
+			_putchar(str[a]);
+		}
+	}
+	_putchar('\n');
 }
