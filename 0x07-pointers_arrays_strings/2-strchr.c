23ac1f9f26921ce83bfe27256318656b75f9ab6d
cristian <cristaker1927@hotmail.com>
2021-10-18 16:40:54 -0700

    point 2

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _strchr - function to print
+ * @s: integer 1
+ * @c: integer 2
+ * Return: Always 0 (Succes)
+ * **/
+
+char *_strchr(char *s, char c)
+{
+	while (*s != '\0')
+	{
+		if (*s == c)
+		{
+			return (s);
+		}
+		s++;
+	}
+	return (0);
+}
