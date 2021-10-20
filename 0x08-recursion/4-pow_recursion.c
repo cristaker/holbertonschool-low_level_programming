1d4411796c08fdc7d4a6d033c042ca8c65944984
cristian <cristaker1927@hotmail.com>
2021-10-20 15:09:47 -0700

    point 4

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _pow_recursion - function
+ * @x: variable 1
+ * @y: variable 2
+ * Return: 0
+ * **/
+
+int _pow_recursion(int x, int y)
+{
+	if (y > 0)
+	{
+		return (x *  _pow_recursion(x, y - 1));
+	}
+	else if (y < 0)
+	{
+		return (-1);
+	}
+	return (1);	
+}
