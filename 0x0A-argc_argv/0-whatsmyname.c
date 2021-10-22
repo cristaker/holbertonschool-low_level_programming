d5137c1c6d2d59b2941aa1810f95e29352e844b2
cristian <cristaker1927@hotmail.com>
2021-10-22 13:39:49 -0700

    point 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - entrey point
+ *
+ * Return: 0
+ * **/
+
+int main(int agrc, char **agrv)
+{
+	(void)agrc;
+	printf("%s\n", *agrv);
+	return (0);
+}
