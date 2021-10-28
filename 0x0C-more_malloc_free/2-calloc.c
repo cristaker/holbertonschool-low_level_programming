3c22f337ceea93c179cf543c1c7b03bdd2a10365
cristian <cristaker1927@hotmail.com>
2021-10-28 16:28:43 -0700

    point 2

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * _calloc - function
+ * @nmemb: unsigned int
+ * @size: unsigned int
+ * Return: pointer
+ * **/
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	int *ptr
+	unsigned int a;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+
+	ptr = malloc(nmemb * size);
+	
+	if (ptr == NULL)
+		return (NULL);
+
+	for (a = 0; a < (nmemb * size); a++)
+		ptr[a] = 0;
+
+	return (ptr);
+}
