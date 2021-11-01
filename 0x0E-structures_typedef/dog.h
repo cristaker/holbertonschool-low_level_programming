0a3620c75bc183d59bfb9788222a88a5aa47b953
cristian <cristaker1927@hotmail.com>
2021-11-01 15:12:16 -0700

    point 0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG
+#define DOG
+/**
+ * struct dog - first struct for dog
+ * @name: char
+ * @age: float
+ * @owner: char
+ * **/
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+struct dog my_dog;
+
+#endif
