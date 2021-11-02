660ac44db40de15b659acc4aab3f4f7bf99ea58d
cristian <cristaker1927@hotmail.com>
2021-11-02 16:44:35 -0700

    point 6

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _strstr - function
+ * @haystack: pointer to pointer
+ * @needle: pointer to pointer 2
+ * Return: a pointer
+ * **/
+
+char *_strstr(char *haystack, char *needle)
+{
+	int a;
+	int b;
+
+}
