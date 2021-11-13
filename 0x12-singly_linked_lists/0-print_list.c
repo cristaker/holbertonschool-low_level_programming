a2950ec02cfb34dbaab4de327ea51f80e152d59e
cristian <cristaker1927@hotmail.com>
2021-11-13 09:31:51 -0800

    point 0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,18 +1,17 @@
 #include "lists.h"
-/*
+/**
  * print_list - function
  * @h: constant list_t
  * Return: number of nodes
- * */
+ * **/
 
 size_t print_list(const list_t *h)
 {
 	unsigned int number = 0;
 	const list_t *a;
+
 	a = h;
-		
-	
-	while (a != NULL)
+		while (a != NULL)
 	{
 		if (a->str == NULL)
 			printf("[0] nil\n");
