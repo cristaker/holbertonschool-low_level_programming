e0e7627cd0b7ab53afca6247791c3a7f55af1701
cristian <cristaker1927@hotmail.com>
2021-11-17 15:59:29 -0800

    point 4

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ * free_listint - function free list
+ * @head: linked list
+ * Return: void
+ * **/
+
+void free_listint(listint_t *head)
+{
+	listint_t *temporal;
+
+	while ((temporal = head) != NULL)
+	{
+		head = head->next;
+		free(head);
+	}
+}
