452e8293f2f4fb120cacad92c2ac015cf7a945ed
cristian <cristaker1927@hotmail.com>
2021-12-14 18:32:17 -0800

    point 5

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -8,7 +8,30 @@
 
 char *_strstr(char *haystack, char *needle)
 {
-	int a;
-	int b;
+	int a = 0;
+	int b = 0;
+	int c = 0;
 
+	if (needle[0] == '\0')
+	{
+		return (haystack);
+	}
+	for (a = 0; haystack[a] != '\0'; a++)
+	{
+		c = 0;
+		if (haystack[a] == needle[0])
+		{
+			b = a;
+			while (haystack[b] == needle[c])
+			{
+				b++;
+				c++;
+			}
+		}
+		if (needle[c] == '\0')
+		{
+			return (haystack + a);
+		}
+	}
+	return (0);
 }
