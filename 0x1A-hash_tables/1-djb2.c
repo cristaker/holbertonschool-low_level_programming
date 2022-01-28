37819d699a889e6f3509a3109af63b7a3cbd9a64
cristian <cristaker1927@hotmail.com>
2022-01-28 09:29:37 -0800

    point 1

--- 0x1A-hash_tables/1-djb2.c
+++ 0x1A-hash_tables/1-djb2.c
@@ -5,7 +5,7 @@
  * @str: string used to generate hash value
  *
  * Return: hash value
- * */
+ * **/
 
 unsigned long int hash_djb2(const unsigned char *str)
 {
@@ -15,7 +15,7 @@ unsigned long int hash_djb2(const unsigned char *str)
 	hash = 5381;
 	while ((c = *str++))
 	{
-		hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
+		hash = ((hash << 5) + hash) + c;
 	}
 	return (hash);
 }
